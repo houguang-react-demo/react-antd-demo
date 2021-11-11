a215fea6fdba45fbe6d892283bef24d415446e8b
houguang <houguang@sina.cn>
2021-11-12 00:23:17 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, DatePicker} from "antd";
+import {HomeOutlined, SearchOutlined} from "@ant-design/icons"
+
+import './App.less';
 
 function App() {
+    function onchange(date,dateString){
+        console.log(date,dateString)
+    }
+    const { RangePicker } = DatePicker;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <HomeOutlined/>
+        <Button type="primary" icon={<SearchOutlined/>}>按钮</Button>
+        <DatePicker onChange={onchange}/>
+        <RangePicker />
     </div>
   );
 }
